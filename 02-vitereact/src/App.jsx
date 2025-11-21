41b3ed34db944d5c7f2633892a5a0e7afb9f0cde
Ankit Chauhan <44ankitchauhan@gmail.com>
2025-11-21 16:33:58 +0530

    Vite React

---
+++ 02-vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Demo from './Demo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>i will get a greert job</h1>
+      <Demo/>
+    </>
+  )
+}
+
+export default App
---
+++ 02-vitereact/src/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Demo() {
+  return (
+    <div>
+      <h2>lets see is this work here</h2>
+    </div>
+  )
+}
+
+export default Demo
